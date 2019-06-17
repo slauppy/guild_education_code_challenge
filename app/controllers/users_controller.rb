4b7534bae8940375e3c980d816004ed65bd93579
Sara Laupp <salaupp@gmail.com>
2019-06-17 11:17:44 -0600

    Add user creation flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,13 @@ class UsersController < ApplicationController
 
     @other_users = User.where.not(id: user_id)
   end
+
+  def new
+  end
+
+  def create
+    new_user = User.create!(user_name: params[:user_name])
+
+    redirect_to new_user
+  end
 end
